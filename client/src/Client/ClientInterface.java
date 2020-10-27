4413b3b5da2cbb1ef8d4b49a2bc2a817eaf5d09a
Dustin Crosson (drc518) <drc518@mail.usask.ca>
2020-10-27 11:50:29 -0600

    Wrote interface, client class and 1 test for object creation

--- client/src/Client/ClientInterface.java
+++ client/src/Client/ClientInterface.java
@@ -2,37 +2,58 @@ package Client;
 
 public interface ClientInterface {
 
-    //setters for client attributes
-    void setName(String name);
-    void setAge(int age);
-    void setId(int id);
-    void setEmail(String email);
-    void setPhoneNum(String phoneNumber);
-    void setHeight(int height);
-    void setWeight(int weight);
-    void setGoal(double goal);
-    void setCalories(double calories);
-    void setAllergies(String[] allergies);
-    void setInstructor(String instructor);
-    void setOrganization(String organization);
-    void setComment(String[] comment);
-    //void controller(Controller);
+  // setters for client attributes
+  void setName(String name);
 
+  void setAge(int age);
 
-    //getters for client
-    String getName();
-    int getId();
-    int getAge();
-    String getEmail();
-    String getPhoneNum();
-    int getHeight();
-    int getWeight();
-    double getGoal();
-    double getCalories();
-    String[] getAllergies();
-    String getInstructor();
-    String getOrganization();
-    //void controller();
-    String[] getComment();
+  void setId(int id);
 
+  void setEmail(String email);
+
+  void setPhoneNum(String phoneNumber);
+
+  void setHeight(int height);
+
+  void setWeight(int weight);
+
+  void setGoal(double goal);
+
+  void setCalories(double calories);
+
+  void setAllergies(String[] allergies);
+
+  void setInstructor(String instructor);
+
+  void setOrganization(String organization);
+
+  void setComment(String[] comment);
+  // void controller(Controller);
+
+  // getters for client
+  String getName();
+
+  int getId();
+
+  int getAge();
+
+  String getEmail();
+
+  String getPhoneNum();
+
+  int getHeight();
+
+  int getWeight();
+
+  double getGoal();
+
+  double getCalories();
+
+  String[] getAllergies();
+
+  String getInstructor();
+
+  String getOrganization();
+  // void controller();
+  String[] getComment();
 }
--- client/src/Client/ClientTest.java
+++ client/src/Client/ClientTest.java
@@ -4,24 +4,35 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClientTest {
-    @Test
-    void testObjectCreate(){
-        Client client1 = new Client("dustin", "dcr518@usask.ca","Rick", "Golds", 1,
-                29, 177, 182, "306-111-1111", 3000.0, 2000.0, null, null);
-        assertEquals("dustin", client1.getName());
-        assertEquals("dcr518@usask.ca", client1.getEmail());
-        assertEquals("Rick", client1.getInstructor());
-        assertEquals("Golds", client1.getOrganization());
-        assertEquals(1, client1.getId());
-        assertEquals(29, client1.getAge());
-        assertEquals(177, client1.getHeight());
-        assertEquals(182, client1.getWeight());
-        assertEquals("306-111-1111", client1.getPhoneNum());
-        assertEquals(3000.0, client1.getGoal(), 0.001); //delta means accurracy
-        assertEquals(2000.0, client1.getCalories(), 0.001); //delta means accurracy
-        assertNull(client1.getAllergies());
-        assertNull(client1.getComment());
-    }
-
-
+  @Test
+  void testObjectCreate() {
+    Client client1 =
+        new Client(
+            "dustin",
+            "dcr518@usask.ca",
+            "Rick",
+            "Golds",
+            1,
+            29,
+            177,
+            182,
+            "306-111-1111",
+            3000.0,
+            2000.0,
+            null,
+            null);
+    assertEquals("dustin", client1.getName());
+    assertEquals("dcr518@usask.ca", client1.getEmail());
+    assertEquals("Rick", client1.getInstructor());
+    assertEquals("Golds", client1.getOrganization());
+    assertEquals(1, client1.getId());
+    assertEquals(29, client1.getAge());
+    assertEquals(177, client1.getHeight());
+    assertEquals(182, client1.getWeight());
+    assertEquals("306-111-1111", client1.getPhoneNum());
+    assertEquals(3000.0, client1.getGoal(), 0.001); // delta means accurracy
+    assertEquals(2000.0, client1.getCalories(), 0.001); // delta means accurracy
+    assertNull(client1.getAllergies());
+    assertNull(client1.getComment());
+  }
 }
