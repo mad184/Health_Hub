65dba8351477b5a1c7fe2a50bde94d9e8f04cd6f
Dustin Crosson (drc518) <drc518@mail.usask.ca>
2020-10-27 23:06:05 -0600

    updated comments to explain accuracy

--- client/src/Client/ClientTest.java
+++ client/src/Client/ClientTest.java
@@ -5,20 +5,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ClientTest {
   Client client1 =
-          new Client(
-                  "dustin",
-                  "dcr518@usask.ca",
-                  "Rick",
-                  "Golds",
-                  1,
-                  29,
-                  177,
-                  182,
-                  "306-111-1111",
-                  3000.0,
-                  2000.0,
-                  null,
-                  null);
+      new Client(
+          "dustin",
+          "dcr518@usask.ca",
+          "Rick",
+          "Golds",
+          1,
+          29,
+          177,
+          182,
+          "306-111-1111",
+          3000.0,
+          2000.0,
+          null,
+          null);
+
   @Test
   void testObjectCreate() {
     assertEquals("dustin", client1.getName());
@@ -30,19 +31,19 @@ class ClientTest {
     assertEquals(177, client1.getHeight());
     assertEquals(182, client1.getWeight());
     assertEquals("306-111-1111", client1.getPhoneNum());
-    assertEquals(3000.0, client1.getGoal(), 0.001); // delta means accurracy
-    assertEquals(2000.0, client1.getCalories(), 0.001); // delta means accurracy
+    assertEquals(3000.0, client1.getGoal(), 0.0001); // 0.0001 accuracy due to comparison of double
+    assertEquals(2000.0, client1.getCalories(), 0.0001); // 0.0001 accuracy due to comparison of double
     assertNull(client1.getAllergies());
     assertNull(client1.getComment());
   }
 
   @Test
-  void changeName(){
+  void changeName() {
     client1.setName("John");
     assertEquals("John", client1.getName());
   }
 
-  void changeAge(){
+  void changeAge() {
     client1.setAge(13);
     assertEquals(13, client1.getAge());
   }
