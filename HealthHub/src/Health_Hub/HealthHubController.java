3dc91c5bfe7dbe6410c584366f80e9cef121834d
Dustin Crosson (drc518) <drc518@mail.usask.ca>
2020-10-28 14:42:55 -0600

    created file with some empty functions

---
+++ HealthHub/src/Health_Hub/HealthHubController.java
@@ -0,0 +1,35 @@
+package Health_Hub;
+
+public class HealthHubController {
+
+    //constructor
+    public HealthHubController(){
+    }
+
+
+    void addClient(){
+
+    }
+
+    void addInstructor(){
+
+    }
+
+    void addManager(){
+
+    }
+
+    void addOwner(){
+
+    }
+
+    int LogIn(){
+        return 1;
+    }
+
+    int Search(){
+        return 1;
+    }
+
+
+}
