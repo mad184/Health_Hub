24d477fe55e25a25935ec04ada3a8c497996af50
Kenneth Andal <kda052@mail.usask.ca>
2020-10-28 20:14:55 -0600

    Moved uriString into local variable. Added "private" to db_writer. Reformat the code according to java style guide

--- healthhub-project/src/database/Dbms.java
+++ healthhub-project/src/database/Dbms.java
@@ -1,30 +1,22 @@
 package database;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.client.MongoCollection;
-import healthhub.Client;
-import org.bson.Document;
 import org.json.JSONObject;
-import healthhub.HealthHubModel;
-// import client.ClientModel;
-// import staff.InstructorModel;
 
 public class Dbms {
-  Writer db_writer;
-  Reader db_reader;
-  private final String uriString;
+  private final Writer db_writer;
+  private Reader db_reader;
 
   /**
-   * Initializes the cloud database to be used for the healthhub application.
-   * if db does not exist, it will not work
-   * if table does not exist, it will create a new table
+   * Initializes the cloud database to be used for the healthhub application. if db does not exist,
+   * it will not work if table does not exist, it will create a new table
+   *
    * @param userName: username credential to connect to cloud
    * @param passWord: passWord credential to connect to cloud(NOT SECURE!)
    * @param dbName: cloud database to connect. Need to exist
    * @param tableName: table inside the cloud database to connect
    */
   Dbms(String userName, String passWord, String dbName, String tableName) {
-    uriString =
+    String uriString =
         "mongodb+srv://"
             + userName
             + ":"
@@ -37,56 +29,62 @@ public class Dbms {
   }
 
   /**
-   * Calls the createClient from the writer initialization.
-   * This should be called when creating a new client within the healthhub application
+   * Calls the createClient from the writer initialization. This should be called when creating a
+   * new client within the healthhub application
+   *
    * @param uniqueCid: unique client id to create
    * @param newClientInfo: new client information to create
    */
-  public void addClient( int uniqueCid,JSONObject newClientInfo) {
-    db_writer.createClient(uniqueCid,newClientInfo);
+  public void addClient(int uniqueCid, JSONObject newClientInfo) {
+    db_writer.createClient(uniqueCid, newClientInfo);
   }
 
   /**
-   * Calls the createInstructor from the writer initialization.
-   * This should be called when creating a new instructor within the healthhub application
+   * Calls the createInstructor from the writer initialization. This should be called when creating
+   * a new instructor within the healthhub application
+   *
    * @param uniqueIid: unique instructor id to create
    * @param newInstructorInfo: new instructor information to create
    */
-  public void addInstructor( int uniqueIid, JSONObject newInstructorInfo){
-    db_writer.createInstructor(uniqueIid,newInstructorInfo);
+  public void addInstructor(int uniqueIid, JSONObject newInstructorInfo) {
+    db_writer.createInstructor(uniqueIid, newInstructorInfo);
   }
 
   /**
-   * Calls the createManager from the writer initialization.
-   * This should be called when creating a new Manager within the healthhub application
+   * Calls the createManager from the writer initialization. This should be called when creating a
+   * new Manager within the healthhub application
+   *
    * @param uniqueMid: unique manager id to create
    * @param newManagerInfo: new manager information to create
    */
-  public void addManager(int uniqueMid, JSONObject newManagerInfo){
-    db_writer.createManager(uniqueMid,newManagerInfo);
+  public void addManager(int uniqueMid, JSONObject newManagerInfo) {
+    db_writer.createManager(uniqueMid, newManagerInfo);
   }
 
   /**
-   * Calls the removeClient from the writer initialization.
-   * This should be called when removing a client from the healthhub application
+   * Calls the removeClient from the writer initialization. This should be called when removing a
+   * client from the healthhub application
+   *
    * @param uniqueCid: unique client id to remove
    */
-  public void removeClient(int uniqueCid){
+  public void removeClient(int uniqueCid) {
     db_writer.removeClient(uniqueCid);
   }
 
   /**
-   * Calls the removeInstructor from the writer initialization
-   * This should be called when removing an instructor from the healthhub application
+   * Calls the removeInstructor from the writer initialization This should be called when removing
+   * an instructor from the healthhub application
+   *
    * @param uniqueIid: unique instructor id to remove
    */
-  public void removeInstructor(int uniqueIid){
+  public void removeInstructor(int uniqueIid) {
     db_writer.removeInstructor(uniqueIid);
   }
 
   /**
-   * calls the updateClient from the writer initialization.
-   * This should be called when updating client information inside the database.
+   * calls the updateClient from the writer initialization. This should be called when updating
+   * client information inside the database.
+   *
    * @param uniqueCid: Unique Client Id
    * @param clientUpdatedData: client updated data to use in replacing
    */
@@ -95,8 +93,9 @@ public class Dbms {
   }
 
   /**
-   * calls the updateInstructor from the writer initialization.
-   * This should be called when updating instructor information inside the database.
+   * calls the updateInstructor from the writer initialization. This should be called when updating
+   * instructor information inside the database.
+   *
    * @param uniqueIid: Unique id for instructors
    * @param instrUpdatedData: Instructor updated to be use in replacing
    */
@@ -105,12 +104,13 @@ public class Dbms {
   }
 
   /**
-   * calls the updateManager from the writer initialization.
-   * This should be called when updating Manager information inside the database.
+   * calls the updateManager from the writer initialization. This should be called when updating
+   * Manager information inside the database.
+   *
    * @param uniqueMid: unique manager id to find
    * @param managerUpdatedData: Manager updated data to store
    */
   protected void updateManager(int uniqueMid, JSONObject managerUpdatedData) {
-    db_writer.updateManager(uniqueMid,managerUpdatedData);
+    db_writer.updateManager(uniqueMid, managerUpdatedData);
   }
 }
