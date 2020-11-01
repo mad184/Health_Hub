fe16bd162b8c3aa256b00d8a9f63f519315ba8aa
Kenneth Andal <kda052@mail.usask.ca>
2020-11-01 14:22:07 -0600

    Reformat Code to follow Google Java Style Guide

--- healthhub-project/src/database/Reader.java
+++ healthhub-project/src/database/Reader.java
@@ -15,133 +15,120 @@ import java.util.logging.Logger;
 
 import static com.mongodb.client.model.Filters.eq;
 
+public class Reader implements ReadWriteInterface {
 
-public class Reader implements ReadWriteInterface{
+  private final MongoClient SERVER; // server initiation
+  private MongoDatabase mongoDb; // database initiation
+  private MongoCollection<Document>
+      collectionTable; // collection initiation ( i.e Table to initiate )
 
-    private final MongoClient SERVER; // server initiation
-    private MongoDatabase mongoDb; // database initiation
-    private MongoCollection<Document>
-            collectionTable; // collection initiation ( i.e Table to initiate )
+  /**
+   * Constructs the Reader class by instantiating connection to a uriString.
+   *
+   * @param uriString: uriString to connect to
+   * @param dbName: Name of the database to connect to
+   * @param tableName: Name of the collection/table to connect to
+   */
+  public Reader(String uriString, String dbName, String tableName) {
+    Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
+    mongoLogger.setLevel(Level.SEVERE); // set logging to only severe conditions
+    SERVER = MongoClients.create(uriString);
+    mongoDb = SERVER.getDatabase(dbName);
+    collectionTable = mongoDb.getCollection(tableName);
+  }
 
-    /**
-     * Constructs the Reader class by instantiating connection to a uriString.
-     *
-     * @param uriString: uriString to connect to
-     * @param dbName: Name of the database to connect to
-     * @param tableName: Name of the collection/table to connect to
-     */
-    public Reader(String uriString, String dbName, String tableName) {
-        Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
-        mongoLogger.setLevel(Level.SEVERE); // set logging to only severe conditions
-        SERVER = MongoClients.create(uriString);
-        mongoDb = SERVER.getDatabase(dbName);
-        collectionTable = mongoDb.getCollection(tableName);
+  @Override
+  public MongoClient getServerObj() {
+    return SERVER;
+  }
+
+  @Override
+  public MongoDatabase getDbObject() {
+    return mongoDb;
+  }
+
+  @Override
+  public MongoCollection<Document> getCollectionTable() {
+    return collectionTable;
+  }
+
+  @Override
+  public void setDbObject(String dbName) {
+    mongoDb = SERVER.getDatabase(dbName);
+  }
+
+  @Override
+  public void setCollectionTable(String newTable) {
+    collectionTable = mongoDb.getCollection(newTable);
+  }
+
+  @Override
+  public void setCollectionTable(MongoCollection<Document> newMongoCollection) {
+    collectionTable = newMongoCollection;
+  }
+
+  public String readClientData(int uniqueCid) {
+    MongoCollection<Document> previousCollection = getCollectionTable();
+    setCollectionTable("ClientCollection");
+
+    try {
+      Document clientData = collectionTable.find(eq("_id", uniqueCid)).first();
+      assert clientData != null;
+      return clientData.toJson();
+    } catch (NullPointerException npe) {
+      return null;
+    } finally {
+      setCollectionTable(previousCollection);
     }
+  }
 
+  public String readInstructorData(int uniqueIid) {
+    MongoCollection<Document> previousCollection = getCollectionTable();
+    setCollectionTable("InstructorCollection");
 
-    @Override
-    public MongoClient getServerObj() {
-        return SERVER;
+    try {
+      Document instructorData = collectionTable.find(eq("_id", uniqueIid)).first();
+      assert instructorData != null;
+      return instructorData.toJson();
+    } catch (NullPointerException npe) {
+      return null;
+    } finally {
+      setCollectionTable(previousCollection);
     }
+  }
 
-    @Override
-    public MongoDatabase getDbObject() {
-        return mongoDb;
+  public String readManagerData(int uniqueMid) {
+    MongoCollection<Document> previousCollection = getCollectionTable();
+    setCollectionTable("ManagerCollection");
+
+    try {
+      Document managerData = collectionTable.find(eq("_id", uniqueMid)).first();
+      assert managerData != null;
+      return managerData.toJson();
+
+    } catch (NullPointerException npe) {
+      return null;
+    } finally {
+      setCollectionTable(previousCollection);
     }
+  }
 
-    @Override
-    public MongoCollection<Document> getCollectionTable() {
-        return collectionTable;
-    }
+  public static void main(String[] args) {
+    final String realUserName = "test-user";
+    final String realPassWord = "healthhub1";
+    final String dbName = "Dev-General-Database";
+    final String tableName = "testCollection";
 
-    @Override
-    public void setDbObject(String dbName) {
-        mongoDb = SERVER.getDatabase(dbName);
-    }
+    String realUriString =
+        "mongodb+srv://"
+            + realUserName
+            + ":"
+            + realPassWord
+            + "@healthhub-cluster.7y7j0.mongodb.net/"
+            + dbName
+            + "?retryWrites=true&w=majority";
 
-    @Override
-    public void setCollectionTable(String newTable) {
-        collectionTable = mongoDb.getCollection(newTable);
-    }
-
-    @Override
-    public void setCollectionTable(MongoCollection<Document> newMongoCollection) {
-        collectionTable = newMongoCollection;
-    }
-
-    public String readClientData(int uniqueCid){
-        MongoCollection<Document> previousCollection = getCollectionTable();
-        setCollectionTable("ClientCollection");
-
-        try{
-            Document clientData = collectionTable.find(eq("_id",uniqueCid)).first();
-            assert clientData != null;
-            return clientData.toJson();
-        }
-        catch(NullPointerException npe){
-            return null;
-        }
-
-        finally{
-            setCollectionTable(previousCollection);
-        }
-
-    }
-
-    public String readInstructorData(int uniqueIid){
-        MongoCollection<Document> previousCollection = getCollectionTable();
-        setCollectionTable("InstructorCollection");
-
-        try{
-            Document instructorData = collectionTable.find(eq("_id",uniqueIid)).first();
-            assert instructorData != null;
-            return instructorData.toJson();
-        }
-        catch(NullPointerException npe){
-            return null;
-        }
-
-        finally{
-            setCollectionTable(previousCollection);
-        }
-
-    }
-
-    public String readManagerData(int uniqueMid){
-        MongoCollection<Document> previousCollection = getCollectionTable();
-        setCollectionTable("ManagerCollection");
-
-        try{
-            Document managerData = collectionTable.find(eq("_id",uniqueMid)).first();
-            assert managerData != null;
-            return managerData.toJson();
-
-        }
-        catch(NullPointerException npe){
-            return null;
-        }
-        finally{
-            setCollectionTable(previousCollection);
-        }
-    }
-
-    public static void main (String[] args){
-        final String realUserName = "test-user";
-        final String realPassWord = "healthhub1";
-        final String dbName = "Dev-General-Database";
-        final String tableName = "testCollection";
-
-        String realUriString =
-                "mongodb+srv://"
-                        + realUserName
-                        + ":"
-                        + realPassWord
-                        + "@healthhub-cluster.7y7j0.mongodb.net/"
-                        + dbName
-                        + "?retryWrites=true&w=majority";
-
-        Reader newReader = new Reader(realUriString,dbName,tableName);
-        System.out.println(newReader.readClientData(1));
-    }
-
+    Reader newReader = new Reader(realUriString, dbName, tableName);
+    System.out.println(newReader.readClientData(1));
+  }
 }
