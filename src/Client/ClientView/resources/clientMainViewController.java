5edec9eb16f0fa00a3f1350538d17c09d87839ad
Justynpollard1 <jmp447@usask.ca>
2020-11-02 22:38:51 -0600

    Added onExerciseButtonPush method to mainViewController Added script to change to exerciseView when pushed

--- src/Client/ClientView/resources/clientMainViewController.java
+++ src/Client/ClientView/resources/clientMainViewController.java
@@ -1,4 +1,24 @@
 package Client.ClientView.resources;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
 public class clientMainViewController {
+
+    public void onExerciseButtonPushed(ActionEvent event) throws IOException {
+        Parent exerciseViewParent = FXMLLoader.load(getClass().getResource("clientExerciseView.fxml"));
+        Scene exerciseViewScene = new Scene(exerciseViewParent);
+
+        //Gets stage information
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(exerciseViewScene);
+        window.show();
+    }
 }
