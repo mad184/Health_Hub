ba4ac507fec5eff6475f040e48c2679726507147
mad184 <mad184@usask.ca>
2020-11-09 14:12:23 -0600

    Add a Model to Staff package. Just a method with a constructor. -- is is not functional --

---
+++ src/staff/StaffModel.java
@@ -0,0 +1,13 @@
+package staff;
+
+import staff.StaffController;
+
+public class StaffModel {
+    private StaffController controller;
+
+    //Constructor
+    public StaffModel(StaffController controller){
+
+        this.controller = controller;
+    }
+}
