24ee987f6939747bbd07dbe12bd7c3ee8241557a
Kenneth Andal <kda052@mail.usask.ca>
2020-11-10 01:37:57 -0600

    Created mock Client Class. Deleted the old Client Class

---
+++ healthhub-project/src/client/Client.java
@@ -0,0 +1,13 @@
+package client;
+
+import org.json.JSONObject;
+
+public class Client {
+    public int getUniqueId() {
+        return 0;
+    }
+
+    public JSONObject getJSONData() {
+        return new JSONObject();
+    }
+}
