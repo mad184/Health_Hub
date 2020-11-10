b0b72afe4a72848f9d8c21a840a5d9410a5255e3
Kenneth Andal <kda052@mail.usask.ca>
2020-11-10 16:58:59 -0600

    Add unit testing for Add CIM methods within the healthHub Model

--- healthhub-project/test/unit/HealthHubModelUnitTest.java
+++ healthhub-project/test/unit/HealthHubModelUnitTest.java
@@ -1,10 +1,50 @@
 package unit;
 
+import client.Client;
 import healthhub.HealthHubModel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.jmock.Mockery;
+import staff.Instructor;
+import staff.Manager;
 
-
-
+@TestMethodOrder(OrderAnnotation.class)
 public class HealthHubModelUnitTest {
 
+  static HealthHubModel testHHM = new HealthHubModel();
+  Client testClient = new Client();
+  Manager testManager = new Manager();
+  Instructor testInstructor = new Instructor();
 
+  @Test
+  @Order(1)
+  void testAddClient() {
+
+    Assertions.assertDoesNotThrow(
+        () -> {
+          testHHM.addClient(testClient);
+        });
+  }
+
+  @Test
+  @Order(2)
+  void testAddInstructor() {
+
+    Assertions.assertDoesNotThrow(
+        () -> {
+          testHHM.addInstructor(testInstructor);
+        });
+  }
+
+  @Test
+  @Order(3)
+  void testAddManager() {
+    Assertions.assertDoesNotThrow(
+        () -> {
+          testHHM.addManager(testManager);
+        });
+  }
 }
