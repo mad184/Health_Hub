ef55810acf34bbf955fccf7d1248b5cbbb5a4e48
Kenneth Andal <kda052@mail.usask.ca>
2020-11-10 18:07:16 -0600

    Added more unit test Cases

--- healthhub-project/test/unit/HealthHubModelUnitTest.java
+++ healthhub-project/test/unit/HealthHubModelUnitTest.java
@@ -2,12 +2,8 @@ package unit;
 
 import client.Client;
 import healthhub.HealthHubModel;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.jmock.Mockery;
 import staff.Instructor;
 import staff.Manager;
 
@@ -47,4 +43,67 @@ public class HealthHubModelUnitTest {
           testHHM.addManager(testManager);
         });
   }
+
+    @Test
+    @Order(4)
+    void testLoginRunning() {
+        Assertions.assertDoesNotThrow(
+                () -> {
+                    testHHM.systemLogin("usertest","userpw","Client");
+                    testHHM.systemLogin("usertest","userpw","Instructor");
+                    testHHM.systemLogin("usertest","userpw","Manager");
+                });
+    }
+
+    @Test
+    //@Disabled
+    @Order(5)
+    void testLoginValidCredentials(){
+        int returnedClientCode = testHHM.systemLogin("validLogin","validPassword","Client");
+        int returnedInstrCode = testHHM.systemLogin("validLogin","validPassword","Instructor");
+        int returnedManagerCode = testHHM.systemLogin("validLogin","validPassword","Manager");
+
+        Assertions.assertEquals(returnedClientCode,200);
+        Assertions.assertEquals(returnedInstrCode,200);
+        Assertions.assertEquals(returnedManagerCode,200);
+    }
+
+    @Test
+    //@Disabled
+    @Order(6)
+    void testLoginInvalidCredentials(){
+        int returnedClientCode = testHHM.systemLogin("InvalidLogin","InvalidPassword","Client");
+        int returnedInstrCode = testHHM.systemLogin("InvalidLogin","InvalidPassword","Instructor");
+        int returnedManagerCode = testHHM.systemLogin("InvalidLogin","InvalidPassword","Manager");
+
+        Assertions.assertEquals(returnedClientCode,401);
+        Assertions.assertEquals(returnedInstrCode,401);
+        Assertions.assertEquals(returnedManagerCode,401);
+    }
+
+    @Test
+    //@Disabled
+    @Order(7)
+    void testLoginCIMNotFound(){
+        int returnedClientCode = testHHM.systemLogin("Who is this guy?","validPassword","Client");
+        int returnedInstrCode = testHHM.systemLogin("Who is this guy","validPassword","Instructor");
+        int returnedManagerCode = testHHM.systemLogin("Who is this guy","validPassword","Manager");
+
+        Assertions.assertEquals(returnedClientCode,404);
+        Assertions.assertEquals(returnedInstrCode,404);
+        Assertions.assertEquals(returnedManagerCode,404);
+    }
+
+  @Test
+  // @Disabled
+  @Order(8)
+  void testLoginSelectionInvalid() {
+    int returnedClientCode = testHHM.systemLogin("Who is this guy?", "validPassword", "Hacker");
+    int returnedInstrCode = testHHM.systemLogin("Who is this guy", "validPassword", "Vtuber");
+    int returnedManagerCode = testHHM.systemLogin("Who is this guy", "validPassword", "Shrimp");
+
+    Assertions.assertEquals(returnedClientCode, -1);
+    Assertions.assertEquals(returnedInstrCode, -1);
+    Assertions.assertEquals(returnedManagerCode, -1);
+    }
 }
