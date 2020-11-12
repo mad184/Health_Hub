8057f3d3887a1d6a39c13ff84eec6c2b5d4d824e
Dustin Crosson (drc518) <drc518@mail.usask.ca>
2020-11-12 14:28:18 -0600

    HealthHubSingleton Completed

--- HealthHub/src/Health_Hub/HealthHubSingleton.java
+++ HealthHub/src/Health_Hub/HealthHubSingleton.java
@@ -1,25 +1,38 @@
 package Health_Hub;
 
-import Client.ClientInterface;
-import Staff.ManagerInterface;
-import Staff.Owner;
-
-import java.util.List;
-
 public class HealthHubSingleton {
-    // Discuss/delete on tuesday
-//    List<ClientInterface> clients;
-//    List<ManagerInterface> managers;
-//    List<ManagerInterface> owners;
 
-    //private constructor to ensure no instances are created
-    private HealthHubSingleton() {
+  // attributes
+  private static HealthHubSingleton HealthHub;
+  String organizationName;
 
+
+  // private constructor to ensure no instances are created
+  private HealthHubSingleton(String Name) {
+    organizationName = Name;
+  }
+
+
+  public static void newOrganziation(String name) {
+    //the organization must have a name
+    if (name == null || name.equals("")) {
+      throw new RuntimeException("The name of a ward cannot be null or empty.  " + "It is " + name);
     }
 
-    private static HealthHubSingleton health_hub;
-
-    public static void initialize(){
-
+    // we can only only initialize the organization once
+    else if (HealthHub != null) {
+      throw new RuntimeException("Initialize should only be invoked once.");
     }
+
+    HealthHub = new HealthHubSingleton(name);
+  }
+
+
+  public static HealthHubSingleton getHealthHub() {
+    if (HealthHub == null) {
+      throw new RuntimeException(
+          "The organization must be previously initialized before it can be accessed.");
+    }
+    return HealthHub;
+  }
 }
