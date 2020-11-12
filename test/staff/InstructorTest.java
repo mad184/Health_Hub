dc17a729498374c4cb8aa351874c0a51b767ce7a
Remington Rohel <remington.rohel@usask.ca>
2020-11-12 13:05:17 -0600

    Modified file to follow coding styleguide.

--- test/staff/InstructorTest.java
+++ test/staff/InstructorTest.java
@@ -8,34 +8,24 @@ import org.jmock.Mockery;
 import org.jmock.Expectations;
 
 public class InstructorTest {
-    Mockery context = new Mockery();
+  Mockery context = new Mockery();
 
-    ArrayList<String> clients = new ArrayList<String>();
-    final Controller controller = context.mock(Controller.class);
-    //Object controller = new Object;
-    Instructor instructor1 = new Instructor(
-            "Remington",
-            22,
-            "rar129@usask.ca",
-            42,
-            175,
-            78,
-            clients,
-            "Average Joes",
-            controller,
-            1
-        );
+  ArrayList<String> clients = new ArrayList<String>();
+  final Controller controller = context.mock(Controller.class);
+  // Object controller = new Object;
+  Instructor instructor1 =
+      new Instructor(
+          "Remington", 22, "rar129@usask.ca", 42, 175, 78, clients, "Average Joes", controller, 1);
 
-    @Test
-    void testInstructorCreate() {
-        assertNotNull(instructor1);
-    }
+  @Test
+  void testInstructorCreate() {
+    assertNotNull(instructor1);
+  }
 
-    @Test
-    void changeName() {
+  @Test
+  void changeName() {
 
-        instructor1.setName("Rohel");
-        assertEquals("Rohel", instructor1.getName());
-    }
-
+    instructor1.setName("Rohel");
+    assertEquals("Rohel", instructor1.getName());
+  }
 }
