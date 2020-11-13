423a42e75dc3cfa5a7fc9bbae0974535c7ea7b47
Remington Rohel <remington.rohel@usask.ca>
2020-11-13 11:56:11 -0600

    Modified to follow coding styleguide.

--- src/staff/UserID.java
+++ src/staff/UserID.java
@@ -1,51 +1,54 @@
 package staff;
 
-/**
- * A storage class which stores the name and database ID of a user.
- */
+/** A storage class which stores the name and database ID of a user. */
 public class UserID {
-    private int id;
-    private String name;
+  private int id;
+  private String name;
 
-    /**
-     * Constructs a UserID object.
-     * @param id: Database ID of the user
-     * @param name: Name of the user
-     */
-    public UserID(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+  /**
+   * Constructs a UserID object.
+   *
+   * @param id: Database ID of the user
+   * @param name: Name of the user
+   */
+  public UserID(int id, String name) {
+    this.id = id;
+    this.name = name;
+  }
 
-    /**
-     * Gets the name of the user
-     * @return the name of the user
-     */
-    public String getName() {
-        return this.name;
-    }
+  /**
+   * Gets the name of the user
+   *
+   * @return the name of the user
+   */
+  public String getName() {
+    return this.name;
+  }
 
-    /**
-     * Gets the database ID of the user.
-     * @return the id of the user
-     */
-    public int getId() {
-        return this.id;
-    }
+  /**
+   * Gets the database ID of the user.
+   *
+   * @return the id of the user
+   */
+  public int getId() {
+    return this.id;
+  }
 
-    /**
-     * Changes the name of the user.
-     * @param name: Name of the user
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
+  /**
+   * Changes the name of the user.
+   *
+   * @param name: Name of the user
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    /**
-     * Sets the database ID of the user.
-     * @param id: ID of the user
-     */
-    public void setId(int id) {
-        this.id = id;
-    }
+  /**
+   * Sets the database ID of the user.
+   *
+   * @param id: ID of the user
+   */
+  public void setId(int id) {
+    this.id = id;
+  }
 }
