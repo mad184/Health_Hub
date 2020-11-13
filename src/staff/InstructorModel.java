de6301e30f28d3fb1b8914cb9493ea2789eba4c4
Remington Rohel <remington.rohel@usask.ca>
2020-11-13 14:49:00 -0600

    Added javadocs, and implement some methods.
    Implemented:
    * getClients
    * addClient
    * removeClient

--- src/staff/InstructorModel.java
+++ src/staff/InstructorModel.java
@@ -38,31 +38,64 @@ public class InstructorModel extends StaffModel implements InstructorInterface {
     this.clients = clients;
   }
 
+  /**
+   * Gets the Client list for the Instructor.
+   *
+   * @return List of UserIDs for the Clients
+   */
   @Override
   public List<UserID> getClients() {
-    return null;
+    return this.clients;
   }
 
+  /**
+   * Adds a Client (UserID) to the Instructor's list.
+   *
+   * @param client: UserID of Client to add
+   */
   @Override
   public void addClient(UserID client) {
-
+    this.clients.add(client);
   }
 
+  /**
+   * Removes a Client (UserID) from the Instructor's list.
+   *
+   * @param client: Client to remove
+   */
   @Override
   public void removeClient(UserID client) {
-
+    this.clients.remove(client);
   }
 
+  /**
+   * Adds a comment to a Client's profile.
+   *
+   * @param client: Client receiving the comment
+   * @param comment: The comment itself
+   */
   @Override
   public void addComment(UserID client, String comment) {
 
   }
 
+  /**
+   * Removes a comment from a Client's profile.
+   *
+   * @param client: Client whose profile the comment is on
+   * @param comment: The comment itself
+   */
   @Override
   public void removeComment(UserID client, String comment) {
 
   }
 
+  /**
+   * Gets all the information on a Client.
+   *
+   * @param client: Client to fetch
+   * @return Client object
+   */
   @Override
   public Client getClientInfo(UserID client) {
     return null;
