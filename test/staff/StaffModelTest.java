dc0595979574d76b37c7ceac24d57f529da50746
Remington Rohel <remington.rohel@usask.ca>
2020-11-14 17:30:56 -0600

    Created class for testing StaffModel class.

---
+++ test/staff/StaffModelTest.java
@@ -0,0 +1,16 @@
+package staff;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+import org.jmock.Mockery;
+import org.jmock.Expectations;
+
+/**
+ * Testing class for StaffModel class.
+ */
+public class StaffModelTest {
+    
+
+}
