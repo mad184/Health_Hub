496baf1f60f39987932cead1e1a26ac06ef74937
Remington Rohel <remington.rohel@usask.ca>
2020-11-14 10:01:22 -0600

    Removed method toJson from the Staff Interface.

--- src/staff/StaffInterface.java
+++ src/staff/StaffInterface.java
@@ -1,7 +1,5 @@
 package staff;
 
-import org.json.JSONObject;
-
 public interface StaffInterface {
 
   /**
@@ -108,11 +106,4 @@ public interface StaffInterface {
    * @param orgName: Name of the organization
    */
   void setOrganization(String orgName);
-
-  /**
-   * Converts the information about the Staff member into JSON.
-   *
-   * @return A JSONObject of the Staff member
-   */
-  JSONObject toJson();
 }
