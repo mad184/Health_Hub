7a2cd0b7e3327f1dabb454b16b3d7754ea299e8a
Dustin Crosson (drc518) <drc518@mail.usask.ca>
2020-11-16 10:45:26 -0600

    updated from deleting other folder

--- healthhub-project/src/healthhub/HealthHubController.java
+++ healthhub-project/src/healthhub/HealthHubController.java
@@ -1,5 +1,76 @@
 package healthhub;
 
-public class HealthHubController {
+import client.Client;
+import staff.Instructor;
+import staff.Manager;
 
+public class HealthHubController {
+    private static HealthHubModel model;
+
+    private static HealthHubSingleton healthHubSingleton;
+
+    //constructor takes the model
+    public HealthHubController(HealthHubModel model){
+        HealthHubController.model = model;
+    }
+
+    /**
+     * Makes call the model to pass it the client created
+     * @param Client: of type Client
+     */
+    protected void addClient(Client client){
+        model.addClient(client);
+    }
+
+    /**
+     * Makes call the model to pass it the instructor created
+     * @param instructor: of type Instructor
+     */
+    protected void addInstructor(Instructor instructor){
+        model.addInstructor(instructor);
+    }
+
+    /**
+     * Makes call the model to pass it the manager created
+     * @param manager: of type Manager
+     */
+    protected void addManager(Manager manager){
+        model.addManager(manager);
+    }
+
+    /**
+     * A method to create the Organization,
+     *
+     * @param name: the name of the organization we want to create
+     * @return
+     *     true -> the organization has been created
+     *     false -> the organziation has already been created
+     */
+    protected boolean createOrganization(String name){
+        try{
+            HealthHubSingleton.newOrganziation(name);
+            return true;
+        }
+        catch(RuntimeException e){
+            return false;
+        }
+    }
+
+  /**
+   * This method take the text box entry from the ClientSignUpView, verifys it and then sends it to
+   * the healthHub Controller to be added to the database, Then switches the view to the clients view
+   *
+   * @param userName: the username the client will have
+   * @param passWord: the passWord the client will have
+   * @param userType: the type of user that tried to log in
+   * @return:
+   *    200 -> for loin success
+   *    404 -> for account not found
+   *    401 -> for exists not correct
+   *    500 -> for server error
+   *    -1 -> for unknown error
+   */
+  protected int LogIn(String userName, String passWord, String userType) {
+        return model.systemLogin(userName, passWord, userType);
+    }
 }
---
+++ healthhub-project/src/healthhub/HealthHubModel.java
@@ -0,0 +1,95 @@
+package healthhub;
+
+import client.Client;
+import com.mongodb.MongoException;
+import database.main.Dbms;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import staff.Instructor;
+import staff.Manager;
+
+public class HealthHubModel {
+  private final Dbms database; // actual database that the healthhub model connects
+
+  public HealthHubModel() {
+    // Need to be changed in the future. This is for Development
+    database = new Dbms("test-user", "healthhub1", "Test-General-Database", "testCollection");
+  }
+
+  /**
+   * This method adds/creates the client within the database
+   *
+   * @param client: Client Object to be added within the database
+   */
+  public void addClient(Client client) {
+    database.createClient(client.getUniqueId(), client.getJSONData());
+  }
+
+  /**
+   * This method adds/creates the instructor within the database
+   *
+   * @param instructor: Instructor Object to be added within the database
+   */
+  public void addInstructor(Instructor instructor) {
+    database.createInstructor(instructor.getUniqueId(), instructor.getJSONData());
+  }
+
+  /**
+   * This method adds/creates the manager within the database
+   *
+   * @param manager: Manager Object to be added within the database
+   */
+  public void addManager(Manager manager) {
+    database.createManager(manager.getUniqueId(), manager.getJSONData());
+  }
+
+  /**
+   * This method is used to login to the system. Primarily, it checks the validity of the
+   * Username/Password
+   *
+   * @param realUserName: Username to be checked
+   * @param realPassword: Password to be checked
+   * @param loginSelection: Can be client, manager, or instructor. Use to determine who are you
+   *     logging in as
+   * @return: 200 for successful found. 401 for incorrect password. 404 for not found
+   * @throws IllegalArgumentException: when the loginSelection is invalid. It needs to be Client,
+   *     Manager or Instructor
+   */
+  public int systemLogin(String realUserName, String realPassword, String loginSelection)
+      throws IllegalArgumentException {
+
+    try {
+      JSONArray CIMdata = new JSONArray();
+
+      switch (loginSelection) {
+        case "Client":
+          CIMdata = database.getAllClients();
+          break;
+        case "Instructor":
+          CIMdata = database.getAllInstructor();
+          break;
+        case "Manager":
+          CIMdata = database.getAllManager();
+          break;
+        default:
+          throw new IllegalArgumentException();
+      }
+
+      for (Object eachElement : CIMdata) {
+
+        if (((JSONObject) eachElement).get("UserName").equals(realUserName)
+            && ((JSONObject) eachElement).get("PassWord").equals(realPassword)) {
+          return 200;
+        } else if (((JSONObject) eachElement).get("UserName").equals(realUserName)
+            && !((JSONObject) eachElement).get("PassWord").equals(realPassword)) {
+          return 401;
+        }
+      }
+      return 404;
+    } catch (MongoException me) {
+      return 500;
+    } catch (Exception e) {
+      return -1;
+    }
+  }
+}
