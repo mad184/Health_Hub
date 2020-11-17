8fda4f708bb39c975a1d5f5a248158887a6e67b6
Dustin Crosson (drc518) <drc518@mail.usask.ca>
2020-11-16 19:35:59 -0600

    Subject: Tests for HealthHubAccessSingleton
    Changes:
    - Created 7 tests, all passed at the time of this commit

---
+++ healthhub-project/test/unit/HealthHubAccessSingletonTest.java
@@ -0,0 +1,82 @@
+package unit;
+
+import healthhub.HealthHub;
+
+import staff.Manager;
+import healthhub.HealthHubAccessSingleton;
+
+import org.junit.Rule;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class HealthHubAccessSingletonTest {
+
+        //attributes for testing
+        Manager owner;
+        HealthHub organization;
+
+        @Rule
+        public final ExpectedException expectedException = ExpectedException.none();
+
+        // try to get an instance of an organization that hasn't been created yet, expect a RuntimeException
+
+        @Test
+        @Order(1)
+        void getObjectNotCreated() {
+            assertThrows(RuntimeException.class, () -> {
+                organization = HealthHubAccessSingleton.getHealthHub();
+            });
+        }
+        @Test
+        @Order(2)
+        void testIfOrganizationIsCreated1(){
+            assertFalse(HealthHubAccessSingleton.isOrganizationCreated());
+        }
+
+        @Test
+        @Order(3)
+        void testObjectCreation(){
+            owner = new Manager();
+            assertDoesNotThrow(()->{
+                HealthHubAccessSingleton.newOrganziation("MyGym", owner);
+            });
+        }
+
+        @Test
+        @Order(4)
+        void testCreatedObjectsName(){
+            assertEquals("MyGym", HealthHubAccessSingleton.getOrganizationName());
+        }
+
+        @Test
+        @Order(5)
+        void testCreatedObjectsOwner(){
+            assertTrue(HealthHubAccessSingleton.getOwner() instanceof Manager);
+        }
+
+        @Test
+        @Order(6)
+        void testIfOrganizationIsCreated2(){
+            assertTrue(HealthHubAccessSingleton.isOrganizationCreated());
+        }
+
+        @Test
+        @Order(7)
+        void createMultipleOrganizations() {
+            Manager owner2 = new Manager();
+            assertThrows(RuntimeException.class, () -> {
+                HealthHubAccessSingleton.newOrganziation("Golds", owner2);
+            });
+        }
+}
