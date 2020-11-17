a09b6312912908d6272bef901ff5d0ffb3c03a61
Dustin Crosson (drc518) <drc518@mail.usask.ca>
2020-11-16 22:39:40 -0600

    Subject: Code Clean-up
    Changes:
    - deleted unknown import statements

--- healthhub-project/src/healthhub/OrganizationSignUpController.java
+++ healthhub-project/src/healthhub/OrganizationSignUpController.java
@@ -1,14 +1,8 @@
 package healthhub;
 
-import com.jcraft.jsch.IO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
 import javax.swing.JOptionPane;
 import java.io.IOException;
