a05a5ff032f8c085fa048f34c4d60242a5c62097
Justynpollard1 <jmp447@usask.ca>
2020-11-17 14:34:56 -0600

    Rearranged foodSearchView.fxml, made add buttons for food results.

--- src/Client/ClientView/FoodSearchView/FoodSearchViewController.java
+++ src/Client/ClientView/FoodSearchView/FoodSearchViewController.java
@@ -3,12 +3,19 @@ package Client.ClientView.FoodSearchView;
 import API.APIManager;
 import API.FoodItem;
 import Client.ClientController;
+import Client.ClientView.ClientMainViewController;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class FoodSearchViewController {
@@ -32,6 +39,11 @@ public class FoodSearchViewController {
     clientController = client;
   }
 
+  /**
+   * Searches for food with inputted string
+   * @param event event of search button being pressed
+   * @throws UnirestException exception
+   */
   public void onSearchButtonPressed(ActionEvent event) throws UnirestException {
     ArrayList<FoodItem> results = foodSearch.searchForFoodItem(searchBar.getText());
     topResultName.setText(results.get(0).getFoodName());
@@ -40,5 +52,30 @@ public class FoodSearchViewController {
     secondResultCals.setText(String.valueOf(results.get(1).getCalories()));
     thirdResultName.setText(results.get(2).getFoodName());
     thirdResultCals.setText(String.valueOf(results.get(2).getCalories()));
+  }
+
+
+  public void topResultAddButton(ActionEvent event) throws IOException {
+    // Loads Scene for main view
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("foodTypeSelectView.fxml"));
+    Parent root = loader.load();
+
+    // Gets main view controller and passes client to it
+    foodTypeSelectViewController viewController = loader.getController();
+    viewController.setupScene(clientController);
+
+    Scene viewScene = new Scene(root);
+    // Gets stage information
+    Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    window.setScene(viewScene);
+    window.show();
+  }
+
+  public void secondResultAddButton(){
+
+  }
+
+  public void thirdResultAddButton(){
+
   }
 }
---
+++ src/Client/ClientView/FoodSearchView/foodTypeSelectViewController.java
@@ -0,0 +1,12 @@
+package Client.ClientView.FoodSearchView;
+
+import Client.ClientController;
+
+public class foodTypeSelectViewController {
+
+    private ClientController clientController = new ClientController(null);
+
+    public void setupScene(ClientController client){
+        clientController = client;
+    }
+}
