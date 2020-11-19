174a1a0f31d6730b20a8d801d6be41fdea508f0a
mad184 <mad184@usask.ca>
2020-11-19 11:26:08 -0600

    Create a new interface that must be implemented in InstructorView in addition to the StaffInterface.

---
+++ src/staff/Interfaces/InstructorViewInterface.java
@@ -0,0 +1,65 @@
+package staff.Interfaces;
+
+import javafx.scene.image.Image;
+import staff.UserID;
+
+import java.util.ArrayList;
+
+public interface InstructorViewInterface {
+
+  // -----------SETTERS----------------//
+
+  void setPassword(String password);
+
+  void setId(int id);
+
+  void setGoalWeight(int goalWeight);
+
+  void setGoalCals(int goalCals);
+
+  void setCalories(int calories);
+
+  void setAllergies(ArrayList<String> allergies);
+
+  void setComment(ArrayList<String> comment);
+
+  void setClients(ArrayList<UserID> clients);
+
+  void setBreakFoods(ArrayList<FoodItem> breakFood);
+
+  void setLunchFoods(ArrayList<FoodItem> lunchFoods);
+
+  void setDinnerFoods(ArrayList<FoodItem> dinnerFoods);
+
+  void setSnackFoods(ArrayList<FoodItem> snackFoods);
+
+  void setProfilePicture(Image profilePicture);
+
+  // ----------------GETTERS-------------//
+
+  String getPassword();
+
+  int getId();
+
+  int getGoalWeight();
+
+  int getGoalCals();
+
+  int getCalories();
+
+  ArrayList<String> getAllergies();
+
+  ArrayList<String> getComment();
+
+  ArrayList<UserID> getClients();
+
+  ArrayList<FoodItem> getBreakFoods();
+
+  ArrayList<FoodItem> getLunchFoods();
+
+  ArrayList<FoodItem> getDinnerFoods();
+
+  ArrayList<FoodItem> getSnackFoods();
+
+  Image getProfilePicture();
+}
