56024f6b6c96b3f91aa29cbb0fc77a865ed88518
mad184 <mad184@usask.ca>
2020-11-23 17:16:28 -0600

    Eliminate flags on method onProgressButtonPushed and add method setUpScene() in InstructorProgressViewController (This class is not fully implemented, just added the one class to eliminate red flag)

--- src/staff/InstructorViews/InstructorProgressViewController.java
+++ src/staff/InstructorViews/InstructorProgressViewController.java
@@ -1,4 +1,12 @@
 package staff.InstructorViews;
 
+import staff.Controllers.InstructorController;
+
 public class InstructorProgressViewController {
+
+    InstructorController controller;
+
+    public void setupScene(InstructorController controller) {
+        this.controller = controller;
+    }
 }
