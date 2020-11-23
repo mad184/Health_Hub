9f4fe65b64530086ba13a26acbc3ffc7f1cbd889
mad184 <mad184@usask.ca>
2020-11-23 17:08:46 -0600

    Change class name from InstructorExerciseController to InstructorExerciseViewController

--- src/staff/InstructorViews/InstructorExerciseController.java
+++
@@ -1,4 +0,0 @@
-package staff.InstructorViews;
-
-public class InstructorExerciseController {
-}
---
+++ src/staff/InstructorViews/InstructorExerciseViewController.java
@@ -0,0 +1,4 @@
+package staff.InstructorViews;
+
+public class InstructorExerciseViewController {
+}
