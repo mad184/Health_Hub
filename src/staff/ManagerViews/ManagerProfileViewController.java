d535f3a64cd3d8070ac33ee9337508028663688f
Remington Rohel <remington.rohel@usask.ca>
2020-11-24 17:20:41 -0600

    Copied InstructorViews package into ManagerViews package, and refactored all "Instructor..." names to "Manager..." so the package will build.

---
+++ src/staff/ManagerViews/ManagerProfileViewController.java
@@ -0,0 +1,13 @@
+package staff.ManagerViews;
+
+import staff.Controllers.ManagerController;
+
+public class ManagerProfileViewController {
+
+    private ManagerController controller;
+
+    public void setupScene(ManagerController controller) {
+        this.controller = controller;
+    }
+}
+
