be0fb06ce60b09e6ba7e0d8d5b357334b8e6fc77
Kenneth Andal <kda052@mail.usask.ca>
2020-11-24 15:02:22 -0600

    Modified the Unit Test Case #4 for the assertEquals. The previous code was not following the paremeter convention of AssertEquals ( i.e it was Actual, Expected rather than Expected, Actual ). The 2nd one is the correct way. New line of code for Testing Organization within the Test Case 4

--- test/databaseTests/unit/ReaderUnitTest.java
+++ test/databaseTests/unit/ReaderUnitTest.java
@@ -152,38 +152,49 @@ public class ReaderUnitTest {
   @Test
   @Order(4)
   void testGetAllCIM() {
-    Assertions.assertDoesNotThrow(() -> {
-      realCon.getAllClients();
-      realCon.getAllInstructors();
-      realCon.getAllManagers();
-      realCon.getAllOrganization();
-    } );
+    Assertions.assertDoesNotThrow(
+        () -> {
+          realCon.getAllClients();
+          realCon.getAllInstructors();
+          realCon.getAllManagers();
+          realCon.getAllOrganization();
+        });
 
     JSONArray testClient = realCon.getAllClients();
     JSONArray testInstructors = realCon.getAllInstructors();
     JSONArray testManagers = realCon.getAllManagers();
+    JSONArray testOrganization = realCon.getAllOrganization();
+
     JSONObject actualClientData = testClient.getJSONObject(0);
     JSONObject actualInstructorsData = testInstructors.getJSONObject(0);
     JSONObject actualManagerData = testManagers.getJSONObject(0);
+    JSONObject actualOrganizationData = testOrganization.getJSONObject(0);
+
     JSONObject expectedData = createTestElement();
 
-    Assertions.assertEquals(actualClientData.get("Age"), expectedData.get("Age"));
-    Assertions.assertEquals(actualClientData.get("Name"), expectedData.get("Name"));
+    Assertions.assertEquals(expectedData.get("Age"), actualClientData.get("Age"));
+    Assertions.assertEquals(expectedData.get("Name"), actualClientData.get("Name"));
     Assertions.assertEquals(
-        actualClientData.get("Hololive Friends").toString(),
-        expectedData.get("Hololive Friends").toString());
+        expectedData.get("Hololive Friends").toString(),
+        actualClientData.get("Hololive Friends").toString());
 
-    Assertions.assertEquals(actualInstructorsData.get("Age"), expectedData.get("Age"));
-    Assertions.assertEquals(actualInstructorsData.get("Name"), expectedData.get("Name"));
+    Assertions.assertEquals(expectedData.get("Age"), actualInstructorsData.get("Age"));
+    Assertions.assertEquals(expectedData.get("Name"), actualInstructorsData.get("Name"));
     Assertions.assertEquals(
-        actualInstructorsData.get("Hololive Friends").toString(),
-        expectedData.get("Hololive Friends").toString());
+        expectedData.get("Hololive Friends").toString(),
+        actualInstructorsData.get("Hololive Friends").toString());
 
-    Assertions.assertEquals(actualManagerData.get("Age"), expectedData.get("Age"));
-    Assertions.assertEquals(actualManagerData.get("Name"), expectedData.get("Name"));
+    Assertions.assertEquals(expectedData.get("Age"), actualManagerData.get("Age"));
+    Assertions.assertEquals(expectedData.get("Name"), actualManagerData.get("Name"));
     Assertions.assertEquals(
-        actualManagerData.get("Hololive Friends").toString(),
-        expectedData.get("Hololive Friends").toString());
+        expectedData.get("Hololive Friends").toString(),
+        actualManagerData.get("Hololive Friends").toString());
+
+    Assertions.assertEquals(expectedData.get("Age"), actualOrganizationData.get("Age"));
+    Assertions.assertEquals(expectedData.get("Name"), actualOrganizationData.get("Name"));
+    Assertions.assertEquals(
+        expectedData.get("Hololive Friends").toString(),
+        actualOrganizationData.get("Hololive Friends").toString());
   }
 
   // Delete everything
