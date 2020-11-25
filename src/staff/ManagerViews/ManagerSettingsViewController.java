eb773df694b62bfb8826bdfd6efdc75b6c087745
Remington Rohel <remington.rohel@usask.ca>
2020-11-24 18:16:37 -0600

    Fixed file name in onBackButtonPressed.

--- src/staff/ManagerViews/ManagerSettingsViewController.java
+++ src/staff/ManagerViews/ManagerSettingsViewController.java
@@ -93,7 +93,7 @@ public class ManagerSettingsViewController {
     // Goes to main view scene when back button is pushed
     public void onBackButtonPressed(ActionEvent event) throws IOException {
         // Loads Scene for main view
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("InstructorMainView.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ManagerMainView.fxml"));
         Parent root = loader.load();
 
         // Gets main view controller and passes client to it
