623a5e8662d1b0c7ceea97b6b7672fd056132e29
mad184 <mad184@usask.ca>
2020-11-30 10:28:35 -0600

    Fix import on junit so it does not user wild card "*" to import assertions

--- test/staff/IntegrationTesting/ManagerModelControllerTest.java
+++ test/staff/IntegrationTesting/ManagerModelControllerTest.java
@@ -7,7 +7,11 @@ import staff.UserID;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ManagerModelControllerTest {
 
