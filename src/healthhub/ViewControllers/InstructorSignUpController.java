57c85881fddbaee66349a4a67559d6ee2021cab2
Dustin Crosson (drc518) <drc518@mail.usask.ca>
2020-11-29 19:53:31 -0600

    Subject: form Validation/refactor
    Changes:
    - import statments cleaned up
    - REGEX:
       - organization just has to be not empty
       - looks for a first and last name
       - phone number looks for something of the format #-###-###-####
          - password looks for 6 charcters long, no spaces but any speical characters or numbers or digits are fine
          _ email is anything of the format example@example.com or example@example.ca

--- src/healthhub/ViewControllers/InstructorSignUpController.java
+++ src/healthhub/ViewControllers/InstructorSignUpController.java
@@ -1,25 +1,12 @@
 package healthhub.ViewControllers;
 
-import Client.Client;
-import Client.ClientView.ClientMainViewController;
 import database.EmptyQueryException;
 import healthhub.HealthHubController;
 import healthhub.Views.View;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-import org.json.JSONObject;
-import staff.Controllers.InstructorController;
-import staff.InstructorViews.InstructorMainViewController;
 import staff.Models.InstructorModel;
-
-
 import javax.swing.JOptionPane;
 import java.io.IOException;
 
@@ -63,8 +50,8 @@ public class InstructorSignUpController {
             JOptionPane.showMessageDialog(null, "age could not be read");
             View.goToView("../../Staff/InstructorViews/instructorMainView.fxml", event);
         }
-        //regex looks for any number of white space
-        if (!(name.length() > 0) || name.matches("^ *$")) {
+        //regex looks for a string space string, meaning users must enter a first and last name
+        if (!(name.length() > 0) || !name.matches("^([a-z]|[A-Z])+\\s([a-z]|[A-Z])+$")) {
             JOptionPane.showMessageDialog(null, "A Name is required");
             View.goToView("../../Staff/InstructorViews/instructorMainView.fxml", event);
         } else if (!(age > 0) || !(age < 150)) {
@@ -72,67 +59,70 @@ public class InstructorSignUpController {
             View.goToView("../../Staff/InstructorViews/instructorMainView.fxml", event);
         }
 
-        //regex looks for empty spaces entered
-        else if (!(email.length() > 0) || email.matches("^ *$")) {
+        //regex looks for a email in the format of anything@anything.ca or anything@anything.com
+        else if (!(email.length() > 0) || !email.matches("^.*[@]{1}.*(.ca|.com)$")) {
             JOptionPane.showMessageDialog(null, "A Email is required");
             View.goToView("../../Staff/InstructorViews/instructorMainView.fxml", event);
         }
 
-        //regex looks for empty spaces entered
-        else if (!(organization.length() > 0) || organization.matches("^ *$")) {
+        //needs to not be empty
+        else if (!(organization.length() > 0)) {
             JOptionPane.showMessageDialog(null, "A userName is required");
             View.goToView("../../Staff/InstructorViews/instructorMainView.fxml", event);
         }
 
-        //regex looks for empty spaces entered
-        else if (!(passWord.length() > 0) || passWord.matches("^ *$")) {
+        // min length of 6, Regex looks for any spaces in the password that is one string, no spaces w/ special characters,characters,numbers
+        else if (!(passWord.length() > 5) || passWord.matches("^ *$") || !passWord.matches("^(\\w|\\D|\\d|\\W)*$")) {
             JOptionPane.showMessageDialog(null, "A Password is required");
             View.goToView("../../Staff/InstructorViews/instructorMainView.fxml", event);
         }
-        //regex looks for empty spaces entered
-        else if (!(phoneNumber.length() > 0) || phoneNumber.matches("^ *$")) {
+        //regex looks anyting of the format #-###-###-####
+        else if (!(phoneNumber.length() > 0) || !phoneNumber.matches("^[0-9][-]{1}[0-9]{3}[-]{1}[0-9]{3}[-]{1}[0-9]{4}$")) {
             JOptionPane.showMessageDialog(null, "A phone number is required");
             View.goToView("../../Staff/InstructorViews/instructorMainView.fxml", event);
-        }
-////        //For testing the outputs manually
-//        System.out.println("Start Manual output Test for Instructor Sign Up input");
-//        System.out.println("name: " + name);
-//        System.out.println("age: " + age);
-//        System.out.println("phoneNumber: " + phoneNumber);
-//        System.out.println("organziation: " + organization);
-//        System.out.println("email: " + email);
-//        System.out.println("passWord: " + passWord);
-//        System.out.println("finsished manual input testing");
-
-        int uniqueId = HealthHubController.getUniqueID();
-        InstructorModel newInstructor = new InstructorModel(name,
-                age,
-                email,
-                phoneNumber,
-                0,
-                0,
-                "none",
-                uniqueId,
-                null,
-                "test-user",
-                "healthhub1",
-                "test-user",
-                "Test-General-Database");
-
-
-        int successCode = HealthHubController.addInstructor(uniqueId, newInstructor.toJson());
-
-        if (successCode == 403) {
-            JOptionPane.showMessageDialog(null, "ERROR: Email " + email + " has already been used");
-            View.goToView("../../Staff/InstructorViews/instructorMainView.fxml", event);
-        } else if (successCode == 500) {
-            JOptionPane.showMessageDialog(null, "ERROR: Server Error");
-            View.goToView("../../Staff/InstructorViews/instructorMainView.fxml", event);
-        } else if (successCode == 200) {
-            View.goToViewWithUniqueID("../../Staff/InstructorViews/instructorMainView.fxml", event, uniqueId, "Instructor");
         } else {
-            JOptionPane.showMessageDialog(null, "ERROR: Sorry a unknown error occured");
-            View.goToView("../../Staff/InstructorViews/instructorMainView.fxml", event);
+            ////        //For testing the outputs manually
+            //        System.out.println("Start Manual output Test for Instructor Sign Up input");
+            //        System.out.println("name: " + name);
+            //        System.out.println("age: " + age);
+            //        System.out.println("phoneNumber: " + phoneNumber);
+            //        System.out.println("organziation: " + organization);
+            //        System.out.println("email: " + email);
+            //        System.out.println("passWord: " + passWord);
+            //        System.out.println("finsished manual input testing");
+
+            int uniqueId = HealthHubController.getUniqueID();
+            InstructorModel newInstructor =
+                    new InstructorModel(
+                            name,
+                            age,
+                            email,
+                            phoneNumber,
+                            0,
+                            0,
+                            "none",
+                            uniqueId,
+                            null,
+                            "test-user",
+                            "healthhub1",
+                            "test-user",
+                            "Test-General-Database");
+
+            int successCode = HealthHubController.addInstructor(uniqueId, newInstructor.toJson());
+
+            if (successCode == 403) {
+                JOptionPane.showMessageDialog(null, "ERROR: Email " + email + " has already been used");
+                View.goToView("../../Staff/InstructorViews/instructorMainView.fxml", event);
+            } else if (successCode == 500) {
+                JOptionPane.showMessageDialog(null, "ERROR: Server Error");
+                View.goToView("../../Staff/InstructorViews/instructorMainView.fxml", event);
+            } else if (successCode == 200) {
+                View.goToViewWithUniqueID(
+                        "../../Staff/InstructorViews/instructorMainView.fxml", event, uniqueId, "Instructor");
+            } else {
+                JOptionPane.showMessageDialog(null, "ERROR: Sorry a unknown error occured");
+                View.goToView("../../Staff/InstructorViews/instructorMainView.fxml", event);
+            }
         }
     }
 }
