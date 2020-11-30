fc4f599d774f65729cc6625ec6c4020d8708e61c
mad184 <mad184@usask.ca>
2020-11-30 02:13:28 -0600

    Change the import on junit so it does not use wild cards to import -- following google format

--- test/staff/InstructorModelControllerTest.java
+++ test/staff/InstructorModelControllerTest.java
@@ -7,7 +7,9 @@ import staff.Models.InstructorModel;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class InstructorModelControllerTest {
 
