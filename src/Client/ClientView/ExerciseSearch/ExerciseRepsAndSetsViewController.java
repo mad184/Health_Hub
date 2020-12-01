8d2af3e60c3cd66911db99ae610130d55d89426a
Justynpollard1 <jmp447@usask.ca>
2020-12-01 15:39:35 -0600

    added setup method to ExerciseRepsAndSetsViewController Changed color of exerciseRepsAndSetsView.fxml to match color scheme

--- src/Client/ClientView/ExerciseSearch/ExerciseRepsAndSetsViewController.java
+++ src/Client/ClientView/ExerciseSearch/ExerciseRepsAndSetsViewController.java
@@ -1,4 +1,13 @@
 package Client.ClientView.ExerciseSearch;
 
+import Client.ClientController;
+
 public class ExerciseRepsAndSetsViewController {
+
+    // Controller to hold client information
+    private ClientController clientController = new ClientController(null);
+
+    public void setupScene(ClientController controller) {
+        clientController = controller;
+    }
 }
