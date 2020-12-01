b82ffb2cc9ba43231e8cb30dcbd81f2c7b19bca2
Justynpollard1 <jmp447@usask.ca>
2020-12-01 15:23:20 -0600

    Added search results to gui with add buttons

---
+++ src/Client/ClientView/ExerciseSearch/ExerciseRepsAndSetsViewController.java
@@ -0,0 +1,4 @@
+package Client.ClientView.ExerciseSearch;
+
+public class ExerciseRepsAndSetsViewController {
+}
--- src/Client/ClientView/ExerciseSearch/ExerciseSearchViewController.java
+++ src/Client/ClientView/ExerciseSearch/ExerciseSearchViewController.java
@@ -5,9 +5,14 @@ import API.ExerciseItem;
 import Client.ClientController;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -22,6 +27,12 @@ public class ExerciseSearchViewController implements Initializable {
     private @FXML
     final TextField searchBar = new TextField();
 
+    //VBoxes for results
+    @FXML
+    private final VBox exerciseNameVBox = new VBox();
+    @FXML
+    private final VBox addButtonVBox = new VBox();
+
     //Array list initialized for results
     private ArrayList<ExerciseItem> results = new ArrayList<>();
 
@@ -33,10 +44,34 @@ public class ExerciseSearchViewController implements Initializable {
     }
 
     public void onSearchButtonPressed(ActionEvent event) throws UnirestException {
+        exerciseNameVBox.getChildren().removeAll();
+        addButtonVBox.getChildren().removeAll();
         results = apiManager.findExerciseSearchMatches(searchBar.getText());
         for (int i = 0; i < results.size(); i++) {
-            System.out.println(results.get(i));
+            Label exerciseLabel = new Label(results.get(i).getExerciseName());
+            exerciseLabel.setTextFill(Color.web("#ddd9d9"));
+            Button addButton = new Button();
+            addButton.setText("+");
+            addButton.setStyle(
+                    "-fx-border-color: #9643a9; " +
+                            "-fx-border-radius: 30; " +
+                            "-fx-background-color: #2a0033; " +
+                            "-fx-background-radius: 30");
+            addButton.setPrefSize(33, 33);
+            int finalI = i;
+            addButton.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent e) {
+                    addButtonPressed(e, results.get(finalI));
+                }
+            });
+            exerciseNameVBox.getChildren().add(exerciseLabel);
+            addButtonVBox.getChildren().add(addButton);
         }
+    }
+
+    public void addButtonPressed(ActionEvent event, ExerciseItem item) {
+        
     }
 
     @Override
