2f12356355867fedbb3375a5bf1a6ab8fcdbbf20
Justynpollard1 <jmp447@usask.ca>
2020-12-01 16:11:31 -0600

    linked fxml buttons and input fields to coresponding ones in controller

--- src/Client/ClientView/ExerciseSearch/ExerciseRepsAndSetsViewController.java
+++ src/Client/ClientView/ExerciseSearch/ExerciseRepsAndSetsViewController.java
@@ -8,9 +8,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-import java.awt.*;
 import java.io.IOException;
 
 public class ExerciseRepsAndSetsViewController {
