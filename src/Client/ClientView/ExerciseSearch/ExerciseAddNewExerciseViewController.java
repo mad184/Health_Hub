92aaabaff47f538503889900f41f10215ce5e95d
Justynpollard1 <jmp447@usask.ca>
2020-12-01 20:26:30 -0600

    Add to ExerciseAddNewExerciseViewController -> Added method to go back to main exercise view -> Added method to create new exercise from input fields and add exercise to client Added to exerciseAddNewExerciseView.fxml ->Connected all input fields and buttons to coresponding objects in controller

--- src/Client/ClientView/ExerciseSearch/ExerciseAddNewExerciseViewController.java
+++ src/Client/ClientView/ExerciseSearch/ExerciseAddNewExerciseViewController.java
@@ -1,13 +1,19 @@
 package Client.ClientView.ExerciseSearch;
 
+import API.ExerciseItem;
 import Client.ClientController;
+import Client.ClientToDB;
+import Client.ClientView.ClientExerciseViewController;
 import database.EmptyQueryException;
+import database.JsonObjectException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,6 +21,17 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ExerciseAddNewExerciseViewController implements Initializable {
+
+    //Connection to database object
+    ClientToDB DB = new ClientToDB();
+
+    //Exercise info textFields
+    @FXML
+    TextField exerciseNameInput = new TextField();
+    @FXML
+    TextField setInput = new TextField();
+    @FXML
+    TextField repInput = new TextField();
 
     private ClientController controller = new ClientController(null);
 
@@ -25,7 +42,6 @@ public class ExerciseAddNewExerciseViewController implements Initializable {
 
     /**
      * Goes back to main view
-     *
      * @param event of back button being pushed
      */
     public void onBackButtonPressed(ActionEvent event) throws IOException, EmptyQueryException {
@@ -35,6 +51,38 @@ public class ExerciseAddNewExerciseViewController implements Initializable {
 
         // Gets main view controller and passes client to it
         ExerciseSearchViewController viewController = loader.getController();
+        viewController.setupScene(controller);
+
+        Scene viewScene = new Scene(root);
+        // Gets stage information
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(viewScene);
+        window.show();
+    }
+
+    public void onExerciseButtonPushed(ActionEvent event) throws JsonObjectException, EmptyQueryException, IOException {
+        //Creates new exercise Item
+        ExerciseItem exerciseItem = new ExerciseItem(
+                exerciseNameInput.getText(),
+                Integer.parseInt(setInput.getText()),
+                Integer.parseInt(repInput.getText()));
+
+        //Adds Exercise to client
+        controller.addExercise(exerciseItem);
+
+        //Updates client in database
+        DB.updateClient(controller.getClientID(), controller.clientToJson());
+
+        //Goes back to main exercise view
+        goToExerciseView(event);
+    }
+
+    public void goToExerciseView(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../clientExerciseView.fxml"));
+        Parent root = loader.load();
+
+        // Gets main view controller and passes client to it
+        ClientExerciseViewController viewController = loader.getController();
         viewController.setupScene(controller);
 
         Scene viewScene = new Scene(root);
