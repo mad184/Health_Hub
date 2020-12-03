eef90bd791711a0fa7fac30d0d745e6a586a0db3
mad184 <mad184@usask.ca>
2020-12-03 16:02:49 -0600

    Add new import on javafx and implement on search button that will find an Instructor in the database. The add button is not implemented yet

--- src/staff/ManagerViews/ManagerAddInstructorController.java
+++ src/staff/ManagerViews/ManagerAddInstructorController.java
@@ -1,14 +1,19 @@
 package staff.ManagerViews;
 
 
-import API.FoodItem;
+
 import database.EmptyQueryException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import staff.Controllers.ManagerController;
 import staff.StaffToDB;
 
@@ -19,19 +24,41 @@ public class ManagerAddInstructorController {
 
     StaffToDB db;
 
-    // Array list initialized for results
-    ArrayList<FoodItem> results = new ArrayList<>();
+    // TextField
+    @FXML
+    TextField InstructorSearchBar = new TextField();
+
+    @FXML
+    Label resultInstructor = new Label();
 
     private ManagerController controller;
 
+    /**
+     *
+     * @param controller
+     */
     public void setupScene(ManagerController controller) {
         this.controller = controller;
         db = new StaffToDB();
     }
 
+    /**
+     *
+     * @param event
+     * @throws IOException
+     */
     public void onSearchButtonPressed(ActionEvent event) throws IOException{
-        //TODO: implement search
-        //TODO: add in the StaffToDb to get all instructors in the db.
+        JSONArray instructorsJson = db.getAllInstructors();
+        if (!InstructorSearchBar.getText().isEmpty()){
+            for (int i = 0 ; i < instructorsJson.length()-1; i++){
+                JSONObject JsonInstructor = instructorsJson.getJSONObject(i);
+                if (String.valueOf(JsonInstructor.get("email")).equals(InstructorSearchBar.getText())){
+                    resultInstructor.setText("Name: "+String.valueOf(JsonInstructor.get("name")) +" ID: " +
+                            String.valueOf(JsonInstructor.get("_id")) + " Email: " +String.valueOf(JsonInstructor.get("email")));
+                }
+            }
+            //TODO: Maybe add a pop up sail that the search view is empty
+        }
     }
 
     public void onAddButton(ActionEvent event) throws IOException{
