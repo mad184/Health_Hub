8292d0e68ee38eff0dd3083428a2c2035326d50c
mad184 <mad184@usask.ca>
2020-12-03 00:34:53 -0600

    Add new methods - onLoadInstructorList() that will be responsible for displaying the instructors when the page is loaded, and onAddInstructorButtonPressed() method for the "+" button. Also set labels to be used to display the instructors information

--- src/staff/ManagerViews/ManagerInstructorsListController.java
+++ src/staff/ManagerViews/ManagerInstructorsListController.java
@@ -2,10 +2,12 @@ package staff.ManagerViews;
 
 import database.EmptyQueryException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import staff.Controllers.ManagerController;
 
@@ -13,25 +15,46 @@ import java.io.IOException;
 
 public class ManagerInstructorsListController {
 
-    private ManagerController controller;
+  @FXML Label firstInstructor = new Label();
+  @FXML Label secondInstructor = new Label();
+  @FXML Label thirdInstructor = new Label();
+  @FXML Label fourthInstructor = new Label();
 
-    public void setupScene(ManagerController controller) {
-        this.controller = controller;
+  private ManagerController controller;
+
+  public void setupScene(ManagerController controller) {
+    this.controller = controller;
+  }
+
+  /** Method called by the setupScene that will load the instructors in the view if there is any */
+  public void onLoadInstructorList() {
+    if (!controller.getInstructors().isEmpty()) {
+      // TODO: add instructors to the labels so far we can do a fix number, fox Example 4
     }
+  }
 
-    public void onBackButtonPressed(ActionEvent event) throws IOException, EmptyQueryException {
-        // Loads Scene for main view
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("managerMainView.fxml"));
-        Parent root = loader.load();
+  /**
+   * Button that will redirect to a page to add a new Instructor
+   *
+   * @param event The "+" button being pushed
+   */
+  public void onAddInstructorButtonPressed(ActionEvent event) throws IOException {
+    // TODO: implement so its possible to add a new client
+  }
 
-        // Gets main view controller and passes client to it
-        ManagerMainViewController MainViewController = loader.getController();
-        MainViewController.setupScene(controller.getId());
+  public void onBackButtonPressed(ActionEvent event) throws IOException, EmptyQueryException {
+    // Loads Scene for main view
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("managerMainView.fxml"));
+    Parent root = loader.load();
 
-        Scene viewScene = new Scene(root);
-        // Gets stage information
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        window.setScene(viewScene);
-        window.show();
-    }
+    // Gets main view controller and passes client to it
+    ManagerMainViewController MainViewController = loader.getController();
+    MainViewController.setupScene(controller.getId());
+
+    Scene viewScene = new Scene(root);
+    // Gets stage information
+    Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    window.setScene(viewScene);
+    window.show();
+  }
 }
