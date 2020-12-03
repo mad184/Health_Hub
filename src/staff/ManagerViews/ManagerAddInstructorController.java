a67579cf17ec8f167d8165ca89be5e336178921f
mad184 <mad184@usask.ca>
2020-12-03 12:10:09 -0600

    Add two methods - onSearchButtonPressed and onAddButton - Both methods aren't implemented, one is for searching for all instructors in the database and the other is to add the instructor if found

--- src/staff/ManagerViews/ManagerAddInstructorController.java
+++ src/staff/ManagerViews/ManagerAddInstructorController.java
@@ -1,5 +1,7 @@
 package staff.ManagerViews;
 
+import API.APIManager;
+import API.FoodItem;
 import database.EmptyQueryException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
@@ -8,15 +10,32 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import staff.Controllers.ManagerController;
+import staff.StaffToDB;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ManagerAddInstructorController {
+
+    StaffToDB db;
+
+    // Array list initialized for results
+    ArrayList<FoodItem> results = new ArrayList<>();
 
     private ManagerController controller;
 
     public void setupScene(ManagerController controller) {
         this.controller = controller;
+        db = new StaffToDB();
+    }
+
+    public void onSearchButtonPressed(ActionEvent event) throws IOException{
+        //TODO: implement search
+        //TODO: add in the StaffToDb to get all instructors in the db.
+    }
+
+    public void onAddButton(ActionEvent event) throws IOException{
+        //TODO: implement teh action of adding the instructor
     }
 
     public void onBackButtonPressed(ActionEvent event) throws IOException, EmptyQueryException {
