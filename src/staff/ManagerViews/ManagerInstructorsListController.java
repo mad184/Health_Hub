fb2ec0b37d55e4df37a799c972ba511fb36dd97d
mad184 <mad184@usask.ca>
2020-12-03 00:04:47 -0600

    Add new classes for the ManagerInstructorsListController and it .fxml. In this page client will be able to add or remove instructors and see a list of added instructors

---
+++ src/staff/ManagerViews/ManagerInstructorsListController.java
@@ -0,0 +1,37 @@
+package staff.ManagerViews;
+
+import database.EmptyQueryException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import staff.Controllers.ManagerController;
+
+import java.io.IOException;
+
+public class ManagerInstructorsListController {
+
+    private ManagerController controller;
+
+    public void setupScene(ManagerController controller) {
+        this.controller = controller;
+    }
+
+    public void onBackButtonPressed(ActionEvent event) throws IOException, EmptyQueryException {
+        // Loads Scene for main view
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("managerMainView.fxml"));
+        Parent root = loader.load();
+
+        // Gets main view controller and passes client to it
+        ManagerMainViewController MainViewController = loader.getController();
+        MainViewController.setupScene(controller.getId());
+
+        Scene viewScene = new Scene(root);
+        // Gets stage information
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(viewScene);
+        window.show();
+    }
+}
