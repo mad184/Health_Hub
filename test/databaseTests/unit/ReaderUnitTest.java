9ccb81e98c83a807969c429dd99428f5e7bccaa0
Kenneth Andal <kda052@mail.usask.ca>
2020-12-06 18:27:19 -0600

    Changed the credentials to follow the final version of the database Reformatted to follow the google java style guide

--- test/databaseTests/unit/ReaderUnitTest.java
+++ test/databaseTests/unit/ReaderUnitTest.java
@@ -9,7 +9,6 @@ import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
@@ -19,9 +18,9 @@ import java.util.ArrayList;
 
 @TestMethodOrder(OrderAnnotation.class)
 public class ReaderUnitTest {
-  private static final String realUserName = "test-user";
+  private static final String realUserName = "production_user";
   private static final String realPassWord = "healthhub1";
-  private static final String dbName = "Test-General-Database";
+  private static final String dbName = "Test-Production-Database";
   private static final String tableName = "testCollection";
 
   private static final String realUriString =
@@ -200,10 +199,11 @@ public class ReaderUnitTest {
 
   @Test
   @Order(5)
-  void testReadUniqueId(){
-    Assertions.assertDoesNotThrow(()->{
-      realCon.getUniqueId();
-    });
+  void testReadUniqueId() {
+    Assertions.assertDoesNotThrow(
+        () -> {
+          realCon.getUniqueId();
+        });
   }
 
   // Delete everything
