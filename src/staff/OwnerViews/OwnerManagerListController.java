fdc394cc2b7b7ca6f578fba1b8a2a5fc2064d684
Dustin Crosson (drc518) <drc518@mail.usask.ca>
2020-12-06 20:54:19 -0600

    Subject: Bug Fix
    Changes:
    - spelling corrected

--- src/staff/OwnerViews/OwnerManagerListController.java
+++ src/staff/OwnerViews/OwnerManagerListController.java
@@ -108,7 +108,7 @@ public class OwnerManagerListController {
    */
   public void onBackButtonPressed(ActionEvent event) throws IOException, EmptyQueryException {
     // Loads Scene for main view
-    FXMLLoader loader = new FXMLLoader(getClass().getResource("OwnerMainView.fxml"));
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("ownerMainView.fxml"));
     Parent root = loader.load();
 
     // Gets main view controller and passes client to it
